304e509a601fc1af4c7457af1c43a92455e1d372
Diego Cardoso <diegocardoso@UNISC.BR>
2018-04-23 10:00:06 -0300

    Created Assistant Service (new Conversation)

---
+++ Source/Services/Assistant.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * Copyright 2017 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace WatsonSDK\Services;
+
+use WatsonSDK\Common\HttpClient;
+use WatsonSDK\Common\HttpResponse;
+use WatsonSDK\Common\HttpClientConfiguration;
+use WatsonSDK\Common\HttpClientException;
+use WatsonSDK\Common\WatsonService;
+use WatsonSDK\Common\WatsonCredential;
+use WatsonSDK\Common\InvalidParameterException;
+
+use WatsonSDK\Services\Assistant\MessageRequestModel;
+
+/**
+ * Assistant class
+ */
+class Assistant extends WatsonService {
+
+    const BASE_URL = 'https://gateway.watsonplatform.net/assistant/api/v1';
+    const VERSION = '2018-02-16';
+
+    const SORT_BY_NAME_ASC = 'name';
+    const SORT_BY_MODIFIED_ASC = 'modified';
+    const SORT_BY_WORKSPACE_ID_ASC = 'workspace_id';
+
+    const SORT_BY_NAME_DESC = '-name';
+    const SORT_BY_MODIFIED_DESC = '-modified';
+    const SORT_BY_WORKSPACE_ID_DESC = '-workspace_id';
+
+    /**
+     * Send message to Assistant service by using the MessageRequestModel instance
+     * 
+     * @param MessageRequestModel $model
+     * @param $workspace_id string
+     * @param $version string
+     * @return HttpResponse
+     */
+    public function sendMessageByModel(MessageRequestModel $model, $workspace_id, $version = self::VERSION) {
+
+        $config = $this->initConfig();
+        $config->addHeaders($model->getData('header'));
+
+        $config->setData($model->getData());
+
+        $config->setQuery( [ 'version' => $version ] );
+        $config->setMethod(HttpClientConfiguration::METHOD_POST);
+        $config->setType(HttpClientConfiguration::DATA_TYPE_JSON);
+
+        $url = self::BASE_URL."/workspaces/{$workspace_id}/message";
+
+        $config->setURL($url);
+
+        $response = $this->sendRequest($config);
+
+        return $response;
+    }
+
+    /**
+     * Send text message to Assistant service
+     * 
+     * @param $message string
+     * @return HttpResponse
+     */
+    public function sendMessageByText($message, $workspace_id, $version = self::VERSION) {
+
+        $model = new MessageRequestModel($message);
+        return $this->sendMessageByModel($model, $workspace_id, $version);
+    }
+
+    /**
+     * Send message to Assistant service
+     * 
+     * @param  $val MessageRequestModel | string
+     * @return HttpResponse
+     */
+    public function sendMessage($val, $workspace_id, $version = self::VERSION) {
+
+        if($val instanceof MessageRequestModel) {
+            return $this->sendMessageByModel($val, $workspace_id, $version);
+        }
+
+        if(is_string($val)) {
+            return $this->sendMessageByText($val, $workspace_id, $version);
+        }
+
+        throw new InvalidParameterException();
+    }
+
+    /**
+     * List the workspaces associated with a Assistant service instance.
+     * 
+     * @return HttpResponse
+     */
+    public function listWorkspaces($page_limit = NULL, $include_count = NULL, $sort = NULL, $cursor = NULL, $version = self::VERSION) {
+
+        $config = $this->initConfig();
+
+        $config->setQuery([ 'version' => $version ]);
+
+        if(is_null($page_limit) === FALSE && is_integer($page_limit)) {
+            $config->addQuery('page_limit', $page_limit);
+        }
+
+        if(is_null($include_count) === FALSE) {
+            $config->addQuery('include_count', $include_count);
+        }
+
+        if(is_null($sort) === FALSE) {
+            $config->addQuery('sort', $sort);
+        }
+
+        if(is_null($cursor) === FALSE) {
+            $config->addQuery('cursor', $cursor);
+        }
+
+        $config->setMethod(HttpClientConfiguration::METHOD_GET);
+        $config->setType(HttpClientConfiguration::DATA_TYPE_JSON);
+        $config->setURL(self::BASE_URL."/workspaces");
+
+        return $this->sendRequest($config);
+    }
+
+    // Create a workspace based on JSON input. 
+    // You must provide JSON data defining the content of the new workspace. 
+    public function createWorkspace($workspace, $version = self::VERSION) {
+        
+    }
+
+}
---
+++ Source/Services/Assistant/MessageRequestModel.php
@@ -0,0 +1,231 @@
+<?php
+/**
+ * Copyright 2017 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace WatsonSDK\Services\Assistant;
+
+use WatsonSDK\Common\ServiceModel;
+
+/**
+ * Conversation `send request` model
+ */
+class MessageRequestModel extends ServiceModel {
+
+    /**
+     * @name(input)
+     * 
+     * The user input.
+     * 
+     * @var InputDataModel
+     */
+    protected $_input;
+
+    /**
+     * @name(alternate_intents)
+     * 
+     * Whether to return more than one intent. 
+     * Set to true to return all matching intents. 
+     * For example, return all intents when the confidence is not high to allow users to choose their intent. 
+     * The default value is false. 
+     * 
+     * @var boolean
+     */
+    protected $_alternate_intents;
+
+    /**
+     * @name(context)
+     * 
+     * State information for the conversation. 
+     * To maintain state, include the Context object from the previous response when sending multiple requests for the same conversation. 
+     * 
+     * @var ContextModel
+     */
+    protected $_context;
+
+    /**
+     * @array(entities)
+     * 
+     * Entities to use when evaluating the message. 
+     * Include entities from the previous response to continue using those entities rather than detecting entities in the new input. 
+     * 
+     * @var EntityModel[]
+     */
+    protected $_entities;
+
+    /**
+     * @array(intents)
+     * 
+     * Intents to use when evaluating the user input. 
+     * Include the intents from the previous response to continue using those intents rather than trying to recognize intents in the new input. 
+     * 
+     * @var IntentModel[]
+     */
+    protected $_intents;
+
+    /**
+     * @array(output)
+     * 
+     * System output. 
+     * Include the output from the previous response to maintain intermediate informationif you have several requests within the same dialog turn. 
+     * 
+     * @var OutputDataModel
+     */
+    protected $_output;
+
+    /**
+     * Constructor.
+     * 
+     * @param string | InputDataModel $text
+     * @param boolean | NULL $alternate_intents
+     * @param ContextModel | NULL $context
+     * @param EntityModel | NULL $entities
+     * @param RuntimeIntent | NULL $intents
+     * @param OutputDataModel | NULL $output
+     */
+    function __construct($input, $alternate_intents = NULL, ContextModel $context = NULL, array $entities = NULL, array $intents = NULL, OutputDataModel $output = NULL) {
+
+        if(is_string($input)) {
+            $input = new InputDataModel($input);
+        }
+
+        $this->setInput($input);
+
+        $this->setAlternateIntents($alternate_intents);
+
+        $this->setContext($context);
+
+        if(is_null($entities) === FALSE) {
+            $this->setEntities($entities);
+        }
+
+        if(is_null($intents) === FALSE) {
+            $this->setIntents($intents);
+        }
+
+        if(is_null($output) === FALSE) {
+            $this->setOutput($output);
+        }
+    }
+
+    /**
+     * Get user input.
+     * 
+     * @return InputDataModel
+     */
+    public function getInput() {
+        return $this->_input;
+    }
+
+    /**
+     * Set user input.
+     * 
+     * @param $val InputDataModel
+     */
+    public function setInput($val) {
+        $this->_input = $val;
+    }
+
+    /**
+     * Get the value of that whether to return more than one intent.
+     * 
+     * @return boolean
+     */
+    public function getAlternateIntents() {
+        return $this->_alternate_intents;
+    }
+
+    /**
+     * Set the value to indicate that whether to return more than one intent.
+     * 
+     * @param $val boolean
+     */
+    public function setAlternateIntents($val) {
+        $this->_alternate_intents = $val;
+    }
+
+    /**
+     * Set state information for the conversation
+     * 
+     * @return ContextModel
+     */
+    public function getContext() {
+        return $this->_context;
+    }
+
+    /**
+     * Set state information for the conversation
+     * 
+     * @param ContextModel $val
+     */
+    public function setContext($val) {
+        $this->_context = $val;
+    }
+
+    /**
+     * Get entities to use when evaluating the message.
+     * 
+     * @return array
+     */
+    public function getEntities() {
+        return $this->_entities;
+    }
+
+    /**
+     * Set entities to use when evaluating the message.
+     * 
+     * @param $val array
+     */
+    public function setEntities($val) {
+        $this->_entities = $val;
+    }
+
+    /**
+     * Get intents to use when evaluating the user input.
+     * 
+     * @return array
+     */
+    public function getIntents() {
+        return $this->_intents;
+    }
+
+    /**
+     * Set intents to use when evaluating the user input.
+     * 
+     * @param $val array
+     */
+    public function setIntents($val) {
+        $this->_intents = $val;
+    }
+
+    /**
+     * Get system output.
+     * 
+     * @return OutputDataModel
+     */
+    public function getOutput() {
+        return $this->_output;
+    }
+
+    /**
+     * Set system output.
+     * 
+     * @param OutputDataModel $val
+     */
+    public function setOutput($val) {
+        $this->_output = $val;
+    }
+
+}
